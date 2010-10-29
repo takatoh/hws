aada83b5b9dd13a59207646cf2898107f74f8d5d
takatoh <takatoh.m@gmail.com>
2010-10-29 10:55:13 +0900

    Typing functions.

--- hws.hs
+++ hws.hs
@@ -3,19 +3,30 @@ module Main where
 
 main :: IO ()
 main = do program <- getContents
-          insns <- compile program
+          let insns = compile program
           run $ initVM insns
-          
 
+------------------------------------------------------------------------
 
+compile :: String -> [Instruction]
 compile = undefined
 
 
-
+initVM :: [Instruction] -> VM
 initVM = undefined
 
 
-
+run :: VM -> IO ()
 run = undefined
 
+
+------------------------------------------------------------------------
+
+type Instruction = String   -- temporary
+
+
+data VM = VM { stack :: [Int]
+             , heap :: [Int]
+             , inst :: [Instruction]
+             }
 
