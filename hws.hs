d9219ebc43322ecd7ca462a8ece59c41704bc02a
takatoh <takatoh.m@gmail.com>
2010-10-29 13:14:39 +0900

    Implementation of Compiler(2) - instruction parsers.

--- hws.hs
+++ hws.hs
@@ -111,5 +111,227 @@ value = do { v <- many1 vToken
            ; return v
            }
 
+----
 
+-- push
+push :: Parser Instruction
+push = do { wsSpace
+          ; wsSpace
+          ; v <- value
+          ; return (Push (parseInt v))
+          }
+
+-- dup
+dup :: Parser Instruction
+dup = do { wsSpace
+         ; wsLf
+         ; wsSpace
+         ; return Dup
+         }
+
+-- copy
+copy :: Parser Instruction
+copy = do { wsSpace
+          ; wsTab
+          ; wsSpace
+          ; v <- value
+          ; return (Copy (parseInt v))
+          }
+
+-- swap
+swap :: Parser Instruction
+swap = do { wsSpace
+          ; wsLf
+          ; wsTab
+          ; return Swap
+          }
+
+-- discard
+discard :: Parser Instruction
+discard = do { wsSpace
+          ; wsLf
+          ; wsLf
+          ; return Discard
+          }
+
+-- slide
+slide :: Parser Instruction
+slide = do { wsSpace
+           ; wsTab
+           ; wsLf
+           ; v <- value
+           ; return (Slide (parseInt v))
+           }
+
+----
+
+-- add
+add :: Parser Instruction
+add = do { wsTab
+         ; wsSpace
+         ; wsSpace
+         ; wsSpace
+         ; return Add
+         }
+
+-- sub
+sub :: Parser Instruction
+sub = do { wsTab
+         ; wsSpace
+         ; wsSpace
+         ; wsTab
+         ; return Sub
+         }
+
+-- mul
+mul :: Parser Instruction
+mul = do { wsTab
+         ; wsSpace
+         ; wsSpace
+         ; wsLf
+         ; return Mul
+         }
+
+-- div
+div :: Parser Instruction
+div = do { wsTab
+         ; wsSpace
+         ; wsTab
+         ; wsSpace
+         ; return Div
+         }
+
+-- mod
+mod :: Parser Instruction
+mod = do { wsTab
+         ; wsSpace
+         ; wsTab
+         ; wsLf
+         ; return Mod
+         }
+
+----
+
+-- heap_write
+heapWrite :: Parser Instruction
+heapWrite = do { wsTab
+               ; wsTab
+               ; wsSpace
+               ; return HeapWrite
+               }
+
+-- heap_read
+heapRead :: Parser Instruction
+heapRead = do { wsTab
+              ; wsTab
+              ; wsTab
+              ; return HeapRead
+              }
+
+----
+
+-- label
+label :: Parser Instruction
+label = do { wsLf
+           ; wsSpace
+           ; wsSpace
+           ; v <- value
+           ; return (Label (parseLabel v))
+           }
+
+-- call
+call :: Parser Instruction
+call = do { wsLf
+          ; wsSpace
+          ; wsTab
+          ; v <- value
+          ; return (Call (parseLabel v))
+          }
+
+-- jump
+jump :: Parser Instruction
+jump = do { wsLf
+          ; wsSpace
+          ; wsLf
+          ; v <- value
+          ; return (Jump (parseLabel v))
+          }
+
+-- jump_zero
+jumpZero :: Parser Instruction
+jumpZero = do { wsLf
+              ; wsTab
+              ; wsSpace
+              ; v <- value
+              ; return (JumpZero (parseLabel v))
+              }
+
+-- jump_nega
+jumpNega :: Parser Instruction
+jumpNega = do { wsLf
+              ; wsTab
+              ; wsTab
+              ; v <- value
+              ; return (JumpNega (parseLabel v))
+              }
+
+-- return
+wsReturn :: Parser Instruction
+wsReturn = do { wsLf
+              ; wsTab
+              ; wsLf
+              ; return Return
+              }
+
+-- exit
+wsExit :: Parser Instruction
+wsExit = do { wsLf
+            ; wsLf
+            ; wsLf
+            ; return Exit
+            }
+
+-- char_out
+charOut :: Parser Instruction
+charOut = do { wsTab
+             ; wsLf
+             ; wsSpace
+             ; wsSpace
+             ; return CharOut
+             }
+
+-- num_out
+numOut :: Parser Instruction
+numOut = do { wsTab
+            ; wsLf
+            ; wsSpace
+            ; wsTab
+            ; return NumOut
+            }
+
+-- char_in
+charIn :: Parser Instruction
+charIn = do { wsTab
+            ; wsLf
+            ; wsTab
+            ; wsSpace
+            ; return CharIn
+            }
+
+-- num_in
+numIn :: Parser Instruction
+numIn = do { wsTab
+           ; wsLf
+           ; wsTab
+           ; wsTab
+           ; return NumIn
+           }
+
+----
+
+parseInt :: [Token] -> Int
+parseInt = undefined
+
+parseLabel :: [Token] -> WSLabel
+parseLabel = undefined
 
